1c2dcb9e3fd0b8357cd52dc63a6e67493ee670e4
SharonFaith <sharonfaith15@gmail.com>
2020-10-05 22:51:45 +0300

    add code to user service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  apiKey = environment.API_token;
+  userSearchUrl = 'https://api.github.com/search/users';
+  userUrl = 'https://api.github.com/users'
 
-  constructor() { }
+  displayUser(theValue) {
+    return this.http.get(`${this.userSearchUrl}?access_token=${this.apiKey}&q=${theValue}&sort=stars&order=desc`)
+  }
+  userRepos(theValue) {
+    return this.http.get(`${this.userUrl}/${theValue}/repos?access_token=${this.apiKey}`)
+  }
+  constructor(private http: HttpClient) { }
 }
