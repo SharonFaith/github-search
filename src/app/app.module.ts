77ad34fe484826c9e50ce9ff697701ef2c1952d1
SharonFaith <sharonfaith15@gmail.com>
2020-10-05 22:00:11 +0300

    add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { GithubSearchComponent } from './github-search/github-search.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    GithubSearchComponent
+    GithubSearchComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
