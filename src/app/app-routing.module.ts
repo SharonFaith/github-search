dc9a7999a3493aefdfa0255b353877fb44710401
SharonFaith <sharonfaith15@gmail.com>
2020-10-05 22:18:57 +0300

    import component modules to app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { GithubSearchComponent } from './github-search/github-search.component'
+
 
 const routes: Routes = [];
 
