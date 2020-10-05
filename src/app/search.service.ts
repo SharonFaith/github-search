b50f03e74912aa21bd3b914448929247a0daee99
SharonFaith <sharonfaith15@gmail.com>
2020-10-05 22:53:24 +0300

    define code for search service that searches for repositories

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  apiKey = environment.API_token;
+  repoSearchUrl = 'https://api.github.com/search/repositories'
+  
+  
+  displayRepo(theValue) {
+    return this.http.get(`${this.repoSearchUrl}?access_token=${this.apiKey}&q=${theValue}&sort=stars&order=desc`)
+  }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
